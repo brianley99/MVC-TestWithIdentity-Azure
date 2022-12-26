c2395dee1823b1a9e5af93cd31dc566cc57f918d
brianley99 <Brianley99@gmail.com>
2022-12-25 21:37:44 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_TestWithIdentity_Azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
